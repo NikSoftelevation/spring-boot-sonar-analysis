bf9022924c88d727c1bcdf5454d3903f404dae9e
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-09-14 16:56:50 +0530

    Initial Commit

---
+++ spring-boot-sonarqube-analysis/src/main/java/com/spring/sonar/SpringBootSonarqubeAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSonarqubeAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSonarqubeAnalysisApplication.class, args);
+	}
+
+}
---
+++ spring-boot-sonarqube-analysis/src/test/java/com/spring/sonar/SpringBootSonarqubeAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSonarqubeAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
